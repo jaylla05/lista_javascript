0894db4c08a41372a6ea39113e2b070bc23b8c2e
jaylla05 <jayllasaavedra@gmail.com>
2026-06-28 19:27:40 -0300

    Add files via upload
    Crie um programa em Javascript que
    leia uma temperatura em Celsius e converta para
    Fahrenheit, usando a fórmula:
    Exiba o resultado em alert e console.log.

---
+++ 2.js
@@ -0,0 +1,6 @@
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+alert("Temperatura em Fahrenheit: " + fahrenheit);
+console.log("Temperatura em Fahrenheit:", fahrenheit);
