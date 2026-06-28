8100ccc0ab60ef899d8e0656921924b940cd1d2f
jaylla05 <jayllasaavedra@gmail.com>
2026-06-28 19:23:51 -0300

    Add files via upload
    Questão 01. Crie um programa em Javascript que
    leia a base e a altura de um triângulo e calcule sua
    área utilizando a fórmula:
    Exiba o resultado em alert e console.log

---
+++ script.js
@@ -0,0 +1,7 @@
+let base = parseFloat(prompt("Digite a base do triângulo:"));
+let altura = parseFloat(prompt("Digite a altura do triângulo:"));
+
+let area = (base * altura) / 2;
+
+alert("Área do triângulo: " + area);
+console.log("Área do triângulo:", area);
